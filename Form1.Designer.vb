dc1552eada1ec05e33ed168f5c8407031c9123b2
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-12-10 19:39:14 -0300

    versão alfa

---
+++ Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.VenderPacoca = New System.Windows.Forms.Button()
+        Me.money = New System.Windows.Forms.Label()
+        Me.BuyPacoca = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Labelpacoca = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'VenderPacoca
+        '
+        Me.VenderPacoca.Location = New System.Drawing.Point(12, 12)
+        Me.VenderPacoca.Name = "VenderPacoca"
+        Me.VenderPacoca.Size = New System.Drawing.Size(93, 36)
+        Me.VenderPacoca.TabIndex = 0
+        Me.VenderPacoca.Text = "Vender paçoca" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(+R$1)"
+        Me.VenderPacoca.UseVisualStyleBackColor = True
+        '
+        'money
+        '
+        Me.money.AutoSize = True
+        Me.money.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.money.Location = New System.Drawing.Point(354, 223)
+        Me.money.Name = "money"
+        Me.money.Size = New System.Drawing.Size(37, 13)
+        Me.money.TabIndex = 1
+        Me.money.Text = "money"
+        '
+        'BuyPacoca
+        '
+        Me.BuyPacoca.Location = New System.Drawing.Point(111, 12)
+        Me.BuyPacoca.Name = "BuyPacoca"
+        Me.BuyPacoca.Size = New System.Drawing.Size(100, 36)
+        Me.BuyPacoca.TabIndex = 2
+        Me.BuyPacoca.Text = "Comprar paçoca" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(-R$0.7)"
+        Me.BuyPacoca.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.SimuladorDeHacker.My.Resources.Resources.pacoca
+        Me.PictureBox1.Location = New System.Drawing.Point(332, 210)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(19, 13)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Labelpacoca
+        '
+        Me.Labelpacoca.AutoSize = True
+        Me.Labelpacoca.Location = New System.Drawing.Point(354, 210)
+        Me.Labelpacoca.Name = "Labelpacoca"
+        Me.Labelpacoca.Size = New System.Drawing.Size(43, 13)
+        Me.Labelpacoca.TabIndex = 4
+        Me.Labelpacoca.Text = "pacoca"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(403, 245)
+        Me.Controls.Add(Me.Labelpacoca)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.BuyPacoca)
+        Me.Controls.Add(Me.money)
+        Me.Controls.Add(Me.VenderPacoca)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents VenderPacoca As Button
+    Friend WithEvents money As Label
+    Friend WithEvents BuyPacoca As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Labelpacoca As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+
+    Dim dinheiro As Double = 10 'começa no zero
+    Dim pacoca As Integer = 0 'começa no zero
+
+
+    Function UpdateValue() As String
+        money.ForeColor = Color.Black
+        money.Text = "$" & dinheiro
+        '                       |_>$1
+
+        Labelpacoca.Text = "$" & pacoca
+    End Function
+
+    Function SemDinheiro() As String
+        Dim oldcolor As Color
+        oldcolor = money.ForeColor '"backup"
+        money.ForeColor = Color.Crimson
+        money.ForeColor = oldcolor
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'nada d+
+
+        UpdateValue()
+        ' coloquei isso no começo por que ele não mudava pra 0
+        'no começo do jogo, só mostrava o valor padrão "money"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles VenderPacoca.Click
+        'VENDER PAÇOCA
+        dinheiro += 1
+        pacoca -= 1
+        UpdateValue()
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles BuyPacoca.Click
+        'COMPRAR PAÇOCA
+        If dinheiro >= 0.7 Then 'maior ou igual
+            dinheiro -= 0.7
+            pacoca += 1
+            UpdateValue()
+        Else 'caso ao contrário
+            'MessageBox.Show("Sem dinheiro :(")
+            SemDinheiro() 'animação que deixa no dinheiro vermelho
+        End If
+
+    End Sub
+End Class
